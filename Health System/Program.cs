6c5ea4494eee2a8186115e821b4013dfee1e9be3
noodles885 <55859706+noodles885@users.noreply.github.com>
2023-10-04 13:55:26 -0300

    update

--- Health System/Program.cs
+++ Health System/Program.cs
@@ -10,6 +10,64 @@ namespace Health_System
     {
         static void Main(string[] args)
         {
+            int score = 0;
+            int health = 100;
+            int lives = 3;
+            int pointsgained;
+            int healthlost;
+
+            Console.WriteLine("Hud v2.0 Health System");
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key ");
+            Console.ReadKey(true);
+            Console.WriteLine();
+
+            Console.WriteLine("Enter a username");
+            string username = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("=====================");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("=====================");
+
+            Console.ReadKey(true);
+            Console.WriteLine();
+
+            pointsgained = 250;
+            healthlost = 20;
+
+            score = score + pointsgained;
+            health = health - healthlost;
+
+            Console.WriteLine("=====================");
+
+            Console.WriteLine("In Battle the ");
+            Console.WriteLine(username + " gained " + pointsgained + " points.");
+
+            Console.WriteLine("In Battle ");
+            Console.WriteLine(username + " lost " + healthlost + " health.");
+
+            Console.WriteLine("=====================");
+
+            Console.ReadKey(true);
+            Console.WriteLine();
+
+            Console.WriteLine("=====================");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("=====================");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
         }
     }
 }
+
+
