2adadb7e548acda32fd1f3f8ef1d3a499b277744
noodles885 <55859706+noodles885@users.noreply.github.com>
2023-10-11 15:00:15 -0300

    update

--- Health System/Program.cs
+++ Health System/Program.cs
@@ -10,64 +10,46 @@ namespace Health_System
     {
         static void Main(string[] args)
         {
-            int score = 0;
+            
             int health = 100;
+            int healthstaus;
+            int shield = 100;
             int lives = 3;
-            int pointsgained;
-            int healthlost;
+            
 
-            Console.WriteLine("Health System v1.0");
+            Console.WriteLine("Health System v1.1");
 
-
-            Console.WriteLine();
-            Console.WriteLine("Press any key ");
-            Console.ReadKey(true);
-            Console.WriteLine();
 
             Console.WriteLine("Enter a username");
             string username = Console.ReadLine();
             Console.WriteLine();
 
             Console.WriteLine("=====================");
-            Console.WriteLine("Score: " + score);
             Console.WriteLine("Health: " + health);
             Console.WriteLine("Lives: " + lives);
             Console.WriteLine("=====================");
 
             Console.ReadKey(true);
             Console.WriteLine();
-
-            pointsgained = 250;
-            healthlost = 20;
-
-            score = score + pointsgained;
-            health = health - healthlost;
-
-            Console.WriteLine("=====================");
-
-            Console.WriteLine("In Battle the ");
-            Console.WriteLine(username + " gained " + pointsgained + " points.");
-
-            Console.WriteLine("In Battle ");
-            Console.WriteLine(username + " lost " + healthlost + " health.");
-
-            Console.WriteLine("=====================");
-
-            Console.ReadKey(true);
-            Console.WriteLine();
-
-            Console.WriteLine("=====================");
-            Console.WriteLine("Score: " + score);
-            Console.WriteLine("Health: " + health);
-            Console.WriteLine("Lives: " + lives);
-            Console.WriteLine("=====================");
+            
+            
+            Takedamage(-50);
+            
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
 
         }
+
+        public static int Takedamage(int damage)
+        {
+
+            return damage;
+        }
+        
     }
+
 }
 
 
