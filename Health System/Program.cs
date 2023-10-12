a51e00c7f4febb3a22e70e39cf03bd09d9763e9a
noodles885 <55859706+noodles885@users.noreply.github.com>
2023-10-12 18:29:07 -0300

    fixed things

--- Health System/Program.cs
+++ Health System/Program.cs
@@ -10,46 +10,61 @@ namespace Health_System
     {
         static void Main(string[] args)
         {
-            
-            int health = 100;
-            int healthstaus;
-            int shield = 100;
-            int lives = 3;
-            
-
             Console.WriteLine("Health System v1.1");
 
+            Player player = new Player();
+            string username = player.GetUsername();
 
-            Console.WriteLine("Enter a username");
-            string username = Console.ReadLine();
-            Console.WriteLine();
-
-            Console.WriteLine("=====================");
-            Console.WriteLine("Health: " + health);
-            Console.WriteLine("Lives: " + lives);
-            Console.WriteLine("=====================");
-
-            Console.ReadKey(true);
-            Console.WriteLine();
-            
-            
-            Takedamage(-50);
-            
+            player.HUD(username);
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
-
         }
-
-        public static int Takedamage(int damage)
-        {
-
-            return damage;
-        }
-        
     }
 
+    internal class Player
+    {
+        int health = 100;
+        string healthStatus;
+        int shield = 100;
+        int lives = 3;
+        int xp = 0;
+        int level = 1;
+
+        public string GetUsername()
+        {
+            Console.WriteLine("Enter a username");
+            string username = Console.ReadLine();
+            Console.WriteLine();
+            return username;
+        }
+
+        public void HUD(string username)
+        {
+            Console.WriteLine("=====================");
+            Console.WriteLine(username + " Stats");
+            Console.WriteLine("Health: " + health + "%");
+            Console.WriteLine("Health Status: " + GetHealthStatus());
+            Console.WriteLine("Shield: " + shield + "%");
+            Console.WriteLine("Lives: " + lives);
+            Console.WriteLine("XP: " + xp);
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("=====================");
+        }
+
+        private string GetHealthStatus()
+        {
+            if (health <= 10)
+                return "Imminent Danger";
+            else if (health <= 50)
+                return "Badly Hurt";
+            else if (health <= 75)
+                return "Hurt";
+            else if (health <= 90)
+                return "Healthy";
+            else
+                return "Perfect Health";
+        }
+    }
 }
-
-
