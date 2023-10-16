9cbae21047c70a817228d49ad82047bdc0a25265
noodles885 <55859706+noodles885@users.noreply.github.com>
2023-10-16 09:06:26 -0300

    added the test code

--- Health System/Program.cs
+++ Health System/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,15 @@ namespace Health_System
     {
         static void Main(string[] args)
         {
+
+            player.UnitTestHealthSystem();
+            player.UnitTestXPSystem();
+
+
+            Console.WriteLine("=====================");
             Console.WriteLine("Health System v1.1");
+            Console.WriteLine("Nadim Yazbek");
+            Console.WriteLine("=====================");
 
             Player player = new Player();
             string username = player.GetUsername();
@@ -65,6 +74,184 @@ namespace Health_System
                 return "Healthy";
             else
                 return "Perfect Health";
+        }
+
+        static void UnitTestHealthSystem()
+        {
+            Debug.WriteLine("Unit testing Health System started...");
+
+            // TakeDamage()
+
+            // TakeDamage() - only shield
+            shield = 100;
+            health = 100;
+            lives = 3;
+            TakeDamage(10);
+            Debug.Assert(shield == 90);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 3);
+
+            // TakeDamage() - shield and health
+            shield = 10;
+            health = 100;
+            lives = 3;
+            TakeDamage(50);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 60);
+            Debug.Assert(lives == 3);
+
+            // TakeDamage() - only health
+            shield = 0;
+            health = 50;
+            lives = 3;
+            TakeDamage(10);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 40);
+            Debug.Assert(lives == 3);
+
+            // TakeDamage() - health and lives
+            shield = 0;
+            health = 10;
+            lives = 3;
+            TakeDamage(25);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 0);
+            Debug.Assert(lives == 3);
+
+            // TakeDamage() - shield, health, and lives
+            shield = 5;
+            health = 100;
+            lives = 3;
+            TakeDamage(110);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 0);
+            Debug.Assert(lives == 3);
+
+            // TakeDamage() - negative input
+            shield = 50;
+            health = 50;
+            lives = 3;
+            TakeDamage(-10);
+            Debug.Assert(shield == 50);
+            Debug.Assert(health == 50);
+            Debug.Assert(lives == 3);
+
+            // Heal()
+
+            // Heal() - normal
+            shield = 0;
+            health = 90;
+            lives = 3;
+            Heal(5);
+            Debug.Assert(shield == 0);
+            Debug.Assert(health == 95);
+            Debug.Assert(lives == 3);
+
+            // Heal() - already max health
+            shield = 90;
+            health = 100;
+            lives = 3;
+            Heal(5);
+            Debug.Assert(shield == 90);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 3);
+
+            // Heal() - negative input
+            shield = 50;
+            health = 50;
+            lives = 3;
+            Heal(-10);
+            Debug.Assert(shield == 50);
+            Debug.Assert(health == 50);
+            Debug.Assert(lives == 3);
+
+            // RegenerateShield()
+
+            // RegenerateShield() - normal
+            shield = 50;
+            health = 100;
+            lives = 3;
+            RegenerateShield(10);
+            Debug.Assert(shield == 60);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 3);
+
+            // RegenerateShield() - already max shield
+            shield = 100;
+            health = 100;
+            lives = 3;
+            RegenerateShield(10);
+            Debug.Assert(shield == 100);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 3);
+
+            // RegenerateShield() - negative input
+            shield = 50;
+            health = 50;
+            lives = 3;
+            RegenerateShield(-10);
+            Debug.Assert(shield == 50);
+            Debug.Assert(health == 50);
+            Debug.Assert(lives == 3);
+
+            // Revive()
+
+            // Revive()
+            shield = 0;
+            health = 0;
+            lives = 2;
+            Revive();
+            Debug.Assert(shield == 100);
+            Debug.Assert(health == 100);
+            Debug.Assert(lives == 1);
+
+            Debug.WriteLine("Unit testing Health System completed.");
+            Console.Clear();
+        }
+
+        static void UnitTestXPSystem()
+        {
+            Debug.WriteLine("Unit testing XP / Level Up System started...");
+
+            // IncreaseXP()
+
+            // IncreaseXP() - no level up; remain at level 1
+            xp = 0;
+            level = 1;
+            IncreaseXP(10);
+            Debug.Assert(xp == 10);
+            Debug.Assert(level == 1);
+
+            // IncreaseXP() - level up to level 2 (costs 100 xp)
+            xp = 0;
+            level = 1;
+            IncreaseXP(105);
+            Debug.Assert(xp == 5);
+            Debug.Assert(level == 2);
+
+            // IncreaseXP() - level up to level 3 (costs 200 xp)
+            xp = 0;
+            level = 2;
+            IncreaseXP(210);
+            Debug.Assert(xp == 10);
+            Debug.Assert(level == 3);
+
+            // IncreaseXP() - level up to level 4 (costs 300 xp)
+            xp = 0;
+            level = 3;
+            IncreaseXP(315);
+            Debug.Assert(xp == 15);
+            Debug.Assert(level == 4);
+
+            // IncreaseXP() - level up to level 5 (costs 400 xp)
+            xp = 0;
+            level = 4;
+            IncreaseXP(499);
+            Debug.Assert(xp == 99);
+            Debug.Assert(level == 5);
+
+            Debug.WriteLine("Unit testing XP / Level Up System completed.");
+            Console.Clear();
         }
     }
 }
